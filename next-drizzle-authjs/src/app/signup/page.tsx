111d6dc3bc6a3cd9a52dbcaa405e51999c1ad83e
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-07-08 22:39:36 +0300

    feat: Add signin page styles (#5)
    - Add `auth-button.tsx` component for adding pending states for buttons
    - Style the page to match the signup page
    
    ---------
    
    Co-authored-by: David Gomes <davidrafagomes@gmail.com>

--- next-drizzle-authjs/src/app/signup/page.tsx
+++ next-drizzle-authjs/src/app/signup/page.tsx
@@ -1,12 +1,40 @@
+"use client";
+
+import React from "react";
+import { AuthButton } from "@/components/auth/auth-button";
+import { useRouter } from "next/navigation";
+
 export default function SignUpPage() {
+  const router = useRouter();
+
+  const [error, setError] = React.useState<string | undefined>();
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-[#111111] px-6 text-[#b4b4b4]">
       <div className="w-full max-w-md">
         <h1 className="mb-3 text-2xl font-medium text-[#eeeeee]">Sign up</h1>
         <form
           className="flex flex-col gap-3"
-          method="post"
-          action="/api/signup"
+          action={async (formData) => {
+            try {
+              const res = await fetch("/api/signup", {
+                method: "POST",
+                body: formData,
+              });
+
+              if (!res.ok) {
+                throw new Error(res.statusText);
+              }
+
+              router.push("/");
+            } catch (error: unknown) {
+              if (error instanceof Error) {
+                setError(error.message);
+              } else {
+                setError("Something went wrong");
+              }
+            }
+          }}
         >
           <div className="space-y-1">
             <label className="text-sm" htmlFor="name">
@@ -47,13 +75,16 @@ export default function SignUpPage() {
               required
             />
           </div>
-          <button
-            type="submit"
-            className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-lg border border-[#3A3A3A] bg-[#111111] px-4 py-2 text-sm font-medium text-[#B4B4B4] ring-offset-[#111111] transition-colors hover:border-[#484848] hover:bg-[#2A2A2A] hover:text-[#EEEEEE] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#00e599] focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-          >
-            Sign Up
-          </button>
+          {error ? <p className="text-sm text-red-500">{error}</p> : null}
+          <AuthButton>Sign up</AuthButton>{" "}
         </form>
+
+        <div className="mt-4 text-sm text-[#b4b4b4]">
+          Already have an account?{" "}
+          <a href="/signin" className="text-[#00e599] hover:underline">
+            Sign in
+          </a>
+        </div>
       </div>
     </div>
   );
