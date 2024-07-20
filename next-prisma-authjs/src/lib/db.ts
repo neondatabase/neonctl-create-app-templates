3c09d93212388d7c8666bf34dfabd34ad62eaa86
David Gomes <davidrafagomes@gmail.com>
2024-07-20 16:27:13 +0200

    try client/edge

--- next-prisma-authjs/src/lib/db.ts
+++ next-prisma-authjs/src/lib/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
