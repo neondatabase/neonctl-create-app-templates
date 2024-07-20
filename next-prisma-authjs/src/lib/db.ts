e2d278b1c1f8c4cb5412bd2c1aa8ef3ec8f55cb9
David Gomes <davidrafagomes@gmail.com>
2024-07-20 16:00:26 +0200

    try pg worker

--- next-prisma-authjs/src/lib/db.ts
+++ next-prisma-authjs/src/lib/db.ts
@@ -1,9 +1,9 @@
-import { Pool } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@prisma/pg-worker";
+import { PrismaPg } from "@prisma/adapter-pg-worker";
 import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
+const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
