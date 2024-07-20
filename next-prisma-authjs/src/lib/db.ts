f2bb72f6e4aeb933243d3b21e1cb9956b5e17dae
David Gomes <davidrafagomes@gmail.com>
2024-07-20 16:26:56 +0200

    Reapply "Reapply "try to add edge again to next-prisma-authjs""
    This reverts commit 10727a3ae2bd98fef2d9b706cb906decebb31410.

--- next-prisma-authjs/src/lib/db.ts
+++ next-prisma-authjs/src/lib/db.ts
@@ -1,3 +1,9 @@
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+export const prisma = new PrismaClient({ adapter });
