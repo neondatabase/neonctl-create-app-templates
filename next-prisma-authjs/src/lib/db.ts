2cb88ab528fdd351b3b5ae03fb847da1f8d8ff68
David Gomes <davidrafagomes@gmail.com>
2024-07-20 16:33:00 +0200

    Revert "try client/edge"
    This reverts commit 3c09d93212388d7c8666bf34dfabd34ad62eaa86.

--- next-prisma-authjs/src/lib/db.ts
+++ next-prisma-authjs/src/lib/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
